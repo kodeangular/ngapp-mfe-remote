8dc4396c498ca965d56c205f6ed317ec5ff13df2
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-06-29 18:17:22 +0530

    remote

--- projects/mfe1/src/app/app.component.spec.ts
+++ projects/mfe1/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngapp-mfe-remote'`, () => {
+  it(`should have as title 'mfe1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngapp-mfe-remote');
+    expect(app.title).toEqual('mfe1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ngapp-mfe-remote app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mfe1 app is running!');
   });
 });
